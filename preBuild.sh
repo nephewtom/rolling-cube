a553fa32469c4f20549c9ff1089142fea52ae4a2
nephewtom <tomasorti@gmail.com>
2025-04-12 04:31:30 +0200

    Tested on Mac

--- preBuild.sh
+++ preBuild.sh
@@ -1,11 +1,11 @@
 echo "***** Building raylib *****"
 cd raylib/src
-make
+make -j4
 cd -
 cp raylib/src/libraylib.a .
 echo
 echo "***** Building rlImGui *****"
 cd rlImGui
-./build.sh
+make -j4
 cd -
 cp rlImGui/src/librlImGui.a .
